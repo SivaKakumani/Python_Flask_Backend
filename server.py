751a763ced301fdbada5e51ddea07bbe2cb79d70
SIVA KAKUMANI <112908439+SivaKakumani@users.noreply.github.com>
2024-05-15 16:30:45 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+CORS(app, origins="http://192.168.56.1:3000")
+app.config['CORS_HEADERS'] = 'Content-Type' 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'kvsp_paul'
+ 
+mysql = MySQL(app)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Welcome to authentication api</p>"
+
+@app.route("/signup", methods=['GET', 'POST'])
+@cross_origin()
+def login():
+    if request.method == 'POST':
+        username = request.json.get('name')
+        email = request.json.get('email')
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO pygo (name, email) VALUES (%s,%s)", (username,email))
+        mysql.connection.commit()
+        cursor.close()
+        print(username)
+        print(email)
+        return jsonify({'status': "post success"})
+    else:
+         return jsonify({'status': "get success"})
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
